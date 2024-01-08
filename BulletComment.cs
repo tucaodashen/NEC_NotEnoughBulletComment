8b0aa3de8e509ce6926ab75b3eccd3c46ceaa922
C7H4NNaO3S <2769809079@qq.com>
2024-01-08 09:27:35 +0800

    添加项目文件。

---
+++ BulletComment.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+
+class BulletComment
+{
+    private static readonly HttpClient httpClient = new HttpClient();
+    private static bool isRunning = true;
+    private List<string> currentComment;
+    public string json;
+    public List<string> currentBulletComment;
+    public bool running;
+
+    public string urlAPI = "https://api.live.bilibili.com/xlive/web-room/v1/dM/gethistory?roomid=";
+    public string roomID;
+    public int Delay = 500;
+
+
+    public void StartLoop()
+    {   
+        running=true;
+        bool isRunning = true;
+        Task.Run(async () =>
+        {
+            while (isRunning)
+            {
+                await GetDataFromApi();
+                await Task.Delay(TimeSpan.FromMilliseconds(200));
+            }
+        });
+    }
+
+    public void StopLoop()
+    {
+        running = false;
+        isRunning = false;
+    }
+
+    async Task GetDataFromApi()
+    {
+        string apiUrl = urlAPI+roomID;
+        HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+        if (response.IsSuccessStatusCode)
+        {
+            json = await response.Content.ReadAsStringAsync();
+
+            List<string> temp = CookComment(json);
+            if (currentComment!=null)
+            {
+                currentBulletComment = GetNewComment(currentComment, temp);
+            }
+            currentComment = temp;
+        }
+        else
+        {
+            Console.WriteLine("无法从API获取数据：" + response.StatusCode);
+        }
+    }
+
+    public static List<string> CookComment(string data)//处理json数据
+    {
+        List<string> usercomment = new List<string>();
+
+        var jsonObject = JObject.Parse(data);
+        var studentsArray = jsonObject["data"];
+
+        foreach (var VARIABLE in studentsArray["admin"])
+        {
+            string comm = VARIABLE["nickname"] + ":" + VARIABLE["text"];
+            usercomment.Add(comm);
+        }
+        foreach (var VARIABLE in studentsArray["room"])
+        {
+            string comm = VARIABLE["nickname"] + ":" + VARIABLE["text"];
+            usercomment.Add(comm);
+        }
+
+        return usercomment;
+
+    }
+    public static List<string> GetNewComment(List<string> oldComments, List<string> newComments)
+    {
+
+        List<string> differentItems = newComments.Except(oldComments).ToList();
+        return differentItems;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Menus;
+using NotEnoughBulletComment;
+using YourModNamespace;
+using System.Threading;
+
+//请高性能机器人亚托莉保佑这段代码不会出bug吧！(/≧▽≦/)
+
+
+namespace NotEnoughBulletComment
+{
+
+    internal sealed class ModEntry : Mod
+    {
+        private static Timer _timer;
+        BulletComment commentHandel;
+        public override void Entry(IModHelper helper)
+        {
+
+            commentHandel= new BulletComment();
+            TimerCallback callback = new TimerCallback(DisplayComment);
+            _timer = new Timer(callback, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(100));
+
+
+
+            helper.ConsoleCommands.Add("BC_Start", "Launch the BulletComment Display \n\nUsage: BC_Start(NoArguements)", this.BCStart);
+            helper.ConsoleCommands.Add("BC_Stop", "Stop the BulletComment Display \n\nUsage: BC_Stop(NoArguements)", this.BCStop);
+            helper.ConsoleCommands.Add("BC_Delay", "Set the delay count(ms) \n\nUsage: BC_Delay <int>", this.BCSetDelay);
+            helper.ConsoleCommands.Add("BC_ID", "Set roomID \n\nUsage: BC_ID <int>", this.BCSetID);
+            helper.ConsoleCommands.Add("BC_API", "SetAPI \n\nUsage: BC_API <string>", this.BCSetAPI);
+
+        }
+
+        
+        private void DisplayComment(object state)
+        {
+            if (commentHandel.currentBulletComment != null) { 
+                foreach(string cm in commentHandel.currentBulletComment)
+                {
+                    Monitor.Log(cm,LogLevel.Trace);
+                    Game1.chatBox.addMessage(cm, Color.White);
+                    commentHandel.currentBulletComment = null;
+                }
+            }
+        }
+        public void BCStart(string command, string[] args)
+        {
+            if(commentHandel.running==null||commentHandel.running==false)
+            {
+                if(commentHandel.roomID==null)
+                {
+                    Monitor.Log("You haven't set id yet.", LogLevel.Error);
+                }
+                else
+                {
+                    if (commentHandel.Delay <= 50)
+                    {
+                        Monitor.Log("Refresh too fast(Delay<=50)", LogLevel.Error);
+                    }
+                    else
+                    {
+                        commentHandel.StartLoop();
+                    }
+                }
+                
+            }
+            else
+            {
+                Monitor.Log("You can't start when a instance is running.", LogLevel.Error);
+            }
+            
+        }
+        public void BCStop(string command, string[] args)
+        {
+            if (commentHandel.running)
+            {
+                commentHandel.StopLoop();
+            }
+            else
+            {
+                Monitor.Log("You haven't start yet.", LogLevel.Error);
+            }
+        }
+        public void BCSetAPI(string command, string[] args)
+        {
+            commentHandel.urlAPI = args[0];
+            string mes = "You have set your API to " + args[0];
+            Monitor.Log(mes, LogLevel.Info);
+        }
+        public void BCSetID(string command, string[] args)
+        {
+            commentHandel.roomID = args[0];
+            string mes = "You have set your roomID to " + args[0];
+            Monitor.Log(mes, LogLevel.Info);
+        }
+        public void BCSetDelay(string command, string[] args)
+        {
+            commentHandel.Delay = int.Parse(args[0]);
+            string mes = "You have set your Delay to " + args[0];
+            Monitor.Log(mes, LogLevel.Info);
+        }
+
+    }
+}
